b84a33438cfeb673f5d0d361b38eec5f8ac2d1d0
M4nu3l4 <142220899+M4nu3l4@users.noreply.github.com>
2024-11-06 18:05:02 +0100

    Update D3.js

--- eserCicli/eserCicli/assets/js/D3.js
+++ eserCicli/eserCicli/assets/js/D3.js
@@ -216,27 +216,6 @@ if (crewMass < 500) {
   console.log("DANGER! OVERLOAD ALERT: escape from ship now!");
 }
 
-starWarsCharacters[0].mass = 500;
-starWarsCharacters[1].mass = 50;
-
-
-crewMass = 0;  
-for (let i = 0; i < starWarsCharacters.length; i++) {
-  crewMass += starWarsCharacters[i].mass;
-}
-
-// 4. Verifica il nuovo messaggio dopo le modifiche
-if (crewMass < 500) {
-  console.log("Ship is under loaded");
-} else if (crewMass >= 500 && crewMass < 700) {
-  console.log("Ship is half loaded");
-} else if (crewMass >= 700 && crewMass < 900) {
-  console.log("Warning: Load is over 700");
-} else if (crewMass >= 900 && crewMass < 1000) {
-  console.log("Critical Load: Over 900");
-} else if (crewMass >= 1000) {
-  console.log("DANGER! OVERLOAD ALERT: escape from ship now!");
-}
 
 /* ESERCIZIO 8
   Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi effettuare la riassegnazione del valore corrispondente o creare un nuovo array)
